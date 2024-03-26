029303b7478c9b7a6d2eb2ce010df0f86621229a
geunyoung <rlarmsdud23@gmail.com>
2024-03-26 09:45:54 +0900

    upload

--- src/componenets/ProductPage.jsx
+++ src/componenets/ProductPage.jsx
@@ -1,36 +1,51 @@
-import React, {useState, useEffect} from 'react';
+import React,{useState, useEffect} from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { LuDog } from "react-icons/lu";
+import axios from "axios";
+import { PiDogFill } from "react-icons/pi";
+import { API_URL } from '../config/constants';
+import {Button, message} from 'antd';
 
 const ProductPage = () => {
     const {id} = useParams();
     const navigate=useNavigate();
-    const [product, setProduct] = useState(null);
+    const [product, setProduct]=useState(null);
 
-    useEffect(()=>{
-        axios.get(`
-        http://localhost:8000/products/${id}`)
+    const getProduct=()=>{
+        axios.get(`${API_URL}/products/${id}`)
         .then(function(result){
             setProduct(result.data.product)
         })
         .catch(function(error){
             console.error(error)
         })
-    }, [id])
+    }
+    useEffect(()=>{
+        getProduct();
+    }, [])
     console.log(product)
     if(product===null){
         return <h1>상품 정보를 받고 있습니다...</h1>
     }
+    const onClickPurchase=()=>{
+        axios.post(`${API_URL}/purchase/${id}`)
+        .then((result) =>{
+            message.info('결제가 완료 되었습니다.')
+            getProduct();
+        })
+        .catch((error)=>{
+            message.error(`에러가 발생했습니다. ${error.message}`)
+        })
+    }
+    
     return (
         <div className='productpageWrap'>
             <h1>상세페이지</h1>
             <button onClick={() => navigate(-1)} id="back-btn">이전화면</button>
-            <div id='image-box'>
-                <img src={`/${product.imageUrl}`} alt={product.name} />
+            <div id="image-box">
+                <img  src={`${API_URL}/${product.imageUrl}`} alt={product.name} />
             </div>
             <div id="profile-box">
-                <LuDog className='product-avata'/>
+                <PiDogFill className='product-avata' />
                 <span className="product-seller">{product.seller}</span>
             </div>
             <div className="contents-box">
@@ -38,6 +53,7 @@ const ProductPage = () => {
                <div id="price">{product.price}</div>
                <div id="createAt">2024.03.19</div>
                <div id="description">{product.description}</div>
+               <Button type="primary" danger={true} className='payment' onClick={onClickPurchase}disabled={product.soldout===1}>즉시결제하기</Button>
             </div>
         </div>
     );
--- src/componenets/Products.jsx
+++ src/componenets/Products.jsx
@@ -2,15 +2,16 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import {Link} from "react-router-dom"
 import dayjs from 'dayjs';
-import isLeapYear from 'dayjs/plugin/isLeapYear'; // 윤년 판단 플러그인
+
 import 'dayjs/locale/ko'; // 한국어 가져오기
+import { API_URL } from '../config/constants';
 
 
 const Products = () => {
     const [products, setProducts] = useState([])
 
     useEffect(()=>{
-        let url="http://localhost:8000/products"
+        let url=`${API_URL}/products`
         axios.get(url).then((reseult)=>{
             const products=reseult.data.product;
             setProducts(products);
@@ -29,10 +30,12 @@ const Products = () => {
            {
                 products.map((product, idx) =>{
                     return(
+                        
                         <div className="product-card" key={idx}>
+                            {product.soldout === 1 ? (<div className="product-blur"></div>) : null}
                             <Link className='product-link' to={`/productpage/${product.id}`}>
                                 <div>
-                                <img src={process.env.PUBLIC_URL + product.imageUrl} alt={product.name} />
+                                <img src={`${API_URL}/${product.imageUrl}`} alt={product.name} />
                                 </div>
                                 <div className="product-content">
                                     <div className="product-name">
